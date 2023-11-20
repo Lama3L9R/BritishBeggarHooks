872e35c02bff1f696da863677e16807b783b3264
Qumolama <Guzhipei@outlook.com>
2023-11-19 20:18:02 -0800

    update rules

--- app/src/main/java/icu/lama/ukbeggar/hooks/MainActivity.java
+++ app/src/main/java/icu/lama/ukbeggar/hooks/MainActivity.java
@@ -53,19 +53,21 @@ public class MainActivity extends AppCompatActivity {
         final String[] RULES = new String[] {
                 "You MUST obey the following rules to use this software:",
 
-                "1. You are NOT allowed to make any profit with this software",
-                "2. You acknowledge that this software has no relation to Lowiro. Any keyword related to Arcaea is coincidental.",
-                "3. You acknowledge that this software has no relations to Arcaea. Any keyword related to Arcaea is coincidental.",
-                "4. You are not allowed to spread a modified version of this software in any case.",
-                "  a) Modified version means the source code is not the same as the source code published by the author",
-                "  b) If your modification is not related to MainActivity, you are allowed to publish a modified version",
-                "     but only the source code.",
-                "  c) You shall not publish any version of compiled binaries/APK files/production builds.",
+                "1. You are NOT allowed to make any profit with this software.",
+                "2. You acknowledge that this software has no relations to Lowiro. Any keyword related to Lowiro is a coincident.",
+                "3. You acknowledge that this software has no relations to Arcaea. Any keyword related to Arcaea is a coincident.",
+                "4. You are not allowed to spread a modified version of this software if the following conditions are matched: ",
+                "  a) Your modification is related to MainActivity.",
+                "  b) You are publishing anything else than the source code.",
                 "5. You are NOT allowed to publish any tutorials if the following conditions are matched: ",
                 "  a) Your tutorial is guiding the reader/viewer to modify the source code.",
                 "  b) Your tutorial is guiding the reader/viewer to reverse engineering this application",
                 "  c) Your tutorial is guiding the reader/viewer to violate the rules",
                 "6. You acknowledge the library 'libnarchook.so' is closed source, and you are not allowed to reverse engineering.",
+                "   Specifically but not limited to these following actions are prohibited:",
+                "  a) Decompile",
+                "  b) Modify",
+                "  c) Republish modified version nor the original version",
                 "7. THE CONSEQUENCES CAUSED BY THE USE OF THIS SOFTWARE SHALL BE BORNE BY THE USER.",
                 "  a) THE CONSEQUENCES may include but not limited to:",
                 "      i) Your game account get banned",
@@ -77,6 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 "Any violation of the rules will cause the author of this project to suspend updates."
         };
 
+        String HERE = "Make this function return true to represent that you have read the rules and will obey them.";
         return false;
     }
 }
